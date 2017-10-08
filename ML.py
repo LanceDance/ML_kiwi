32c27588428db82238a4964fb444a182187bf50b
petrochvat <petrochvat@gmail.com>
2017-10-08 21:20:44 +0200

    Some new changes with charts and rovnice

--- ML.py
+++ ML.py
@@ -1,36 +1,67 @@
-import time
+import numpy as np
 import matplotlib.pyplot as plt
 import requests
-import numpy as np
-
+plt.get_backend()
+plt.interactive(False)
+plt.figure()
+plt.interactive(False)
 lisx = []
 lisy = []
 
-
-start = time.time()
-for a in range(-10, 10):
+for a in range(-10,10):
         url = 'http://165.227.157.145:8080/api/do_measurement?x=' + str(a)
+        # rl = urllib.request.urlopen('http://165.227.157.145:8080/api/do_measurement?x=' + str(a)).read()
         r = requests.get(url)
         c = r.json()
-        x = c['data']['x']
+        x = int(c['data']['x'])
         lisx.append(x)
-        y = c['data']['y']
+        y = int(c['data']['y'])
         lisy.append(y)
-        if x == 0:
-                print()
-end = time.time()
-axes = plt.gca()
-ymin, ymax = axes.get_ylim()
-# print(lisx)
-# print(lisy)
-print(end - start)
-plt.plot(lisx, lisy)
-plt.show()
 
-peak = np.ptp(lisx,lisy)
+xarr = np.array(lisx)
+yarr = np.array(lisy)
+matrix = np.column_stack((xarr,yarr))
+
+b = matrix[matrix != 0].min()
+rows = np.where(matrix[:, 1] == b)
+peak = matrix[rows]
+h = peak[0][0]
+k = peak[0][1]
 print(peak)
-# plt.savefig('ML.png')
-#
-# res = numpy.linalg.solve(lisx,lisy)
-# print(res)
+print(b)
+print(matrix)
+# z = np.polyfit(xarr,yarr,0)
+# print("{0}x^2 + {1}x^2 ".format(z))
+x = 0
+# chage some things
+vysledek = a * (x - h) ** 2 + k
+print(vysledek)
+
+vysledek2 = vysledek * (x - h) ** 2 + k
+print(vysledek2)
+
+# miny = min(yarr)
+# print(miny)
+# print(matrix)
+
+
+# ymin = min(lisx)
+# xpos = lisy.index(ymin)
+# xmin = xarr[xpos]
+# ymax = max(lisy)
+
+# xpos = lisy.index(ymax)
+# xmas = lisx[xpos]
+# print(ymin,xmin)
+
+
+#vykresleni grafu
+plt.axhline(0, color='black')
+plt.axvline(0, color='black')
+axes = plt.gca()
+axes.get_ylim()
+plt.plot(xarr, yarr)
+
+
+plt.show()
 
