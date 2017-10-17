8cd8aef15fac9dff29d811c55d2fa04dacd33514
petrochvat <petrochvat@gmail.com>
2017-10-17 22:00:53 +0200

    rovnice uz pracuej jak ma

--- ML.py
+++ ML.py
@@ -8,8 +8,13 @@ plt.interactive(False)
 lisx = []
 lisy = []
 
-for a in range(-10,10):
-        url = 'http://165.227.157.145:8080/api/do_measurement?x=' + str(a)
+def find_nearest_vector(array, value):
+  idx = np.array([np.linalg.norm(y+x) for (y,x) in array-value]).argmin()
+  return array[idx]
+
+
+for i in range(-20,20):
+        url = 'http://165.227.157.145:8080/api/do_measurement?x=' + str(i)
         # rl = urllib.request.urlopen('http://165.227.157.145:8080/api/do_measurement?x=' + str(a)).read()
         r = requests.get(url)
         c = r.json()
@@ -17,6 +22,7 @@ for a in range(-10,10):
         lisx.append(x)
         y = int(c['data']['y'])
         lisy.append(y)
+
 
 xarr = np.array(lisx)
 yarr = np.array(lisy)
@@ -26,44 +32,21 @@ b = np.minimum(xarr,yarr)
 b = matrix[matrix[0][1] != 0].min()
 rows = np.where(matrix[:, 1] == b)
 peak = matrix[rows]
-h = peak[0][0]
-k = peak[0][1]
-print(peak)
-print(b)
+h = peak.item(0)
+k = peak.item(1)
+print(h)
+print(k)
 print(matrix)
 z = np.polyfit(xarr,yarr,0)
-# print("{0}x^2 + {1}x^2 ".format(z))
-yzero = 0
-itemindex = np.where(yarr == 0)
-abc = itemindex[0]
-xzero = matrix[abc][0][0]
 
-xyz = None
-vysledek = (xzero - h)**2
+yzero = find_nearest_vector(matrix,0)
 
+xzero = yzero[0]
+yzero = yzero[1]
+print(xzero,yzero)
 
-# vysledek = a * (x - h) ** 2 + k
-# print(vysledek)
-#
-# vysledek2 = vysledek * (x - h) ** 2 + k
-# print(vysledek2)
-
-# miny = min(yarr)
-# print(miny)
-# print(matrix)
-
-
-# ymin = min(lisx)
-# xpos = lisy.index(ymin)
-# xmin = xarr[xpos]
-# ymax = max(lisy)
-
-# xpos = lisy.index(ymax)
-# xmas = lisx[xpos]
-# print(ymin,xmin)
-
-
-# vykresleni grafu
+zavorka = (xzero - h) ** 2
+print('{}y = a({}x - {}h) ** 2 + {}k'.format(yzero,xzero,h,k))
 plt.axhline(0, color='black')
 plt.axvline(0, color='black')
 axes = plt.gca()
